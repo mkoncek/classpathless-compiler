305a6a6a139f9303a71f2aedc306049c1461707c
Marián Konček <marian.koncek@mailbox.org>
2023-06-26 10:21:58 +0200

    Update comments

--- api/src/main/java/io/github/mkoncek/classpathless/api/IdentifiedSource.java
+++ api/src/main/java/io/github/mkoncek/classpathless/api/IdentifiedSource.java
@@ -23,14 +23,15 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 /**
  * Java source file with its fully qualified name.
  */
-@SuppressFBWarnings(value = {"EQ_DOESNT_OVERRIDE_EQUALS"}, justification = "intentionally using equals from superclass")
+@SuppressFBWarnings(value = {"EQ_DOESNT_OVERRIDE_EQUALS"},
+    justification = "intentionally using equals from superclass")
 public class IdentifiedSource extends IdentifiedFile {
     private final Charset charset;
 
     /**
      * @param classIdentifier fully qualified name of class.
      * @param file java source code of class.
-     * @param charset charset of source which was used to get the byte[].
+     * @param charset Character set of source which was used to get the byte[].
      * Default encoding is used if not provided.
      */
     public IdentifiedSource(ClassIdentifier classIdentifier, byte[] file, Charset charset) {
